d0a3090c3db4bf5a207e2718df1a6ea0b3f5bef3
sourav sahu <sahusourav1999.pet@gmail.com>
2024-04-09 02:16:57 +0530

    print from n to 1

---
+++ Basics/printFromNto1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    //Print from N to 1
+    int i = n;
+    while(i >=1) {
+        cout << i << endl;
+        --i;
+    }
+    return 0;
+}
