2add9e20e52127eb34cbb9fe4a938770f3e424fe
sourav sahu <sahusourav1999.pet@gmail.com>
2024-04-09 02:12:04 +0530

    Take input N times and print

---
+++ Basics/TakeinputNtimesAndPrint.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n; 
+    cin >> n;
+    for(int i=1; i<=n; i++) {
+        int x;
+        cin >> x;
+        cout << "value Entered : " << x << endl;
+    }
+    return 0;
+}
