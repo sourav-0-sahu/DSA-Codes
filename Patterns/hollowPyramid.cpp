f14a7aec2699e539d8096329fe7e7b77c0a7711d
sourav sahu <sahusourav1999.pet@gmail.com>
2024-04-11 22:29:16 +0530

    added some spaces to fix the pattern

--- Patterns/hollowPyramid.cpp
+++ Patterns/hollowPyramid.cpp
@@ -13,14 +13,31 @@ using namespace std;
 void printPattern(int n) {
     for(int row = 0; row < n; row++) {
         //for leading space
-        for(int col =0; col < n -1-row; col++) {
-            cout << :" "
+        for(int col =0; col < n-1-row; col++) {
+            cout << " ";
         }
+
+
+        //For spaces
+        for(int col = 0; col < row+1; col++) {
+         
+            if(col == 0 || col == (row+1)-1)
+            {
+                //for the first column and last column
+                //print stars
+                cout << "* ";  //added space here
+            }
+            else {
+                cout << "  "; //added double space here
+            }
+        }
+
+        cout << endl;
     }
 }
 
 int main() {
-    int n = 5;
+    int n = 7;
     printPattern(n);
     return 0;
 }
