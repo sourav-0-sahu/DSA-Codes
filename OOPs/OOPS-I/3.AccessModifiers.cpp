7752d4e776400f4f11b1270554b34b8278c279d0
sourav sahu <sahusourav1999.pet@gmail.com>
2024-04-19 01:13:58 +0530

    Public and Private Data Members of a class

---
+++ OOPs/OOPS-I/3.AccessModifiers.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+    private:
+    //Private Data Members
+    string gfName;
+    
+    public:
+    //Public Data Members
+    int id;
+    string name;
+    int age;
+    int noOfSubjects;
+
+    //Public Member Functions
+    void study() {
+        cout << "Studying" << endl;
+    }
+
+    void bunk() {
+        cout << "Roaming in the streets" << endl;
+    }
+
+    private:
+    void chatting() {
+        cout << "Chatting with Girl Friend" << endl;
+    }
+
+
+};
+
+
+int main() {
+    //Creating an instance of class i.e Object
+    Student s1;
+    //If the data members are public , we can directly Initialize them
+    s1.name = "Sourav Sahu";
+    s1.id = 15;
+    s1.age = 25;
+    s1.noOfSubjects = 6;
+
+    //Accessing Data Members
+    cout << s1.name << endl;
+    cout << s1.age << endl;
+    cout << s1.id << endl;
+    cout << s1.noOfSubjects << endl;
+
+    //Note : we cannot directly access private data members of a class
+    
+    return 0;
+}
