3b1913d17726cf3ae4ead8bae9cd37ae45f54af9
sourav sahu <sahusourav1999.pet@gmail.com>
2024-04-18 22:37:54 +0530

    Accessing Global Variable Using Double Colon

---
+++ OOPs/Basics/2.UsingDoubleColon.cpp
@@ -0,0 +1,16 @@
+//Using double colon to acces global variables
+#include<iostream>
+using namespace std;
+
+int x = 2; //Global Variable
+
+int main() {
+    x=59; //This will modify the global variable x
+
+    int x = 889; //Local Variable
+    cout << "Local Variable x : " << x << endl;
+
+    //Accessing Global variable using double colon(::)
+    cout << "Global Variable x : " << ::x << endl;
+    return 0;
+}
