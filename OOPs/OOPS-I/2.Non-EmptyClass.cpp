1db5748b4013f5fa17412cdc8027a29aa6fdf7d5
sourav sahu <sahusourav1999.pet@gmail.com>
2024-04-19 00:37:27 +0530

    This is a series of commits do watch other Commits

--- OOPs/OOPS-I/2.Non-EmptyClass.cpp
+++ OOPs/OOPS-I/2.Non-EmptyClass.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 class Student {
-    int id;
-    int age;
-    int markOfMathematics;
+    double a; 
+    double b;
+    char c;
 
     void study() {
         cout << "Studying" << endl;
@@ -14,6 +14,17 @@ class Student {
 
 int main() {
 
-    cout << sizeof(Student) << endl; //OUTPUT : 12 bytes
+    cout << sizeof(Student) << endl; //OUTPUT : 24 bytes
+
+    /*
+        sizeof double a = 8 bytes
+        size of double b = 8bytes
+        size of char c = 1byte
+        Total = 8+8+1 =17 bytes
+        Nearest Multiple of 8 which is >= 17  is 24
+        That is why the size of class is 24 bytes
+        Extra 7 bytes that were added is known as padding
+        and padding is done by default by the compiler for quick accessing
+    */
     return 0;
 }
