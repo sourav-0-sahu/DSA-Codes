c2661a82ebd31bc6a8dde549846a6519039f5111
sourav sahu <sahusourav1999.pet@gmail.com>
2024-04-18 22:24:22 +0530

    Local x is defined though global x is present

--- OOPs/Basics/1.GlobalVsLocalVariable.cpp
+++ OOPs/Basics/1.GlobalVsLocalVariable.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int x = 2; //Global Variable
 
 int main() {
+    x=59; //Modifying the Global Variable
     cout << x << endl;
-    cout << "Hello wrold" << endl;
-    
+    int x = 885; //local variable x 
+    cout << x << endl; //Output : 885
     return 0;
 }
