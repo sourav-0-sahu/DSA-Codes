f8cc513d010fd055e696dda0ec6613ae5c1fda5d
sourav sahu <sahusourav1999.pet@gmail.com>
2024-06-29 00:01:18 +0530

    Level Order Traversal

--- Generic & Binary Trees/Implementation.cpp
+++ Generic & Binary Trees/Implementation.cpp
@@ -1,103 +1,147 @@
-#include<iostream>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 
-    Node(int val) {
+    Node(int val)
+    {
         this->data = val;
         this->left = NULL;
         this->right = NULL;
     }
 };
 
-//since this function will return root node of the entire tree
-Node* createTree() {
-    //Data input
+// since this function will return root node of the entire tree
+Node *createTree()
+{
+    // Data input
     int data;
     cout << "Enter Data" << endl;
     cin >> data;
 
-    //Ek parent Node
-    Node* parentNode = new Node(data);
+    // Ek parent Node
+    Node *parentNode = new Node(data);
 
-    //Tree ka left subtree pehele banega 
+    // Tree ka left subtree pehele banega
     parentNode->left = createTree();
-    //Phir tree ka right subtree banega
+    // Phir tree ka right subtree banega
     parentNode->right = createTree();
 
     return parentNode;
 }
 
-//Preorder Traversal
-void preOrderTraversal(Node* currNode) {
-    //Base case
-    if(currNode == NULL) {
+// Preorder Traversal
+void preOrderTraversal(Node *currNode)
+{
+    // Base case
+    if (currNode == NULL)
+    {
         return;
     }
 
-    //Agar base case nahi hai
-    //iska matlab currNode null nahi hai
+    // Agar base case nahi hai
+    // iska matlab currNode null nahi hai
 
-    //N -> prints the data in the current node
+    // N -> prints the data in the current node
     cout << currNode->data << endl;
 
-    //L -> preorder traversal on the left sub tree
+    // L -> preorder traversal on the left sub tree
     preOrderTraversal(currNode->left);
 
-    //R -> preorder traversal on the right
+    // R -> preorder traversal on the right
     preOrderTraversal(currNode->right);
 }
 
-void postOrderTraversal(Node* currNode) {
-    //Base case
-    if(currNode == NULL) {
+void postOrderTraversal(Node *currNode)
+{
+    // Base case
+    if (currNode == NULL)
+    {
         return;
     }
 
-    //Agar base case hit nahi hua
-    //iska matlab currNode ek valid node hai
-    
-    //L
+    // Agar base case hit nahi hua
+    // iska matlab currNode ek valid node hai
+
+    // L
     postOrderTraversal(currNode->left);
-    //R
+    // R
     postOrderTraversal(currNode->right);
-    //N
+    // N
     cout << currNode->data << endl;
 }
 
-void inOrderTraversal(Node* currNode) {
-    //Base case
-    if(currNode == NULL) {
+void inOrderTraversal(Node *currNode)
+{
+    // Base case
+    if (currNode == NULL)
+    {
         return;
     }
 
-    //if not base case hit
-    //L 
+    // if not base case hit
+    // L
     inOrderTraversal(currNode->left);
-    //N
+    // N
     cout << currNode->data << endl;
-    //R
+    // R
     inOrderTraversal(currNode->right);
 }
 
-int main() 
+void levelOrderTraversal(Node *root)
 {
-    Node* root = new Node(10);    
-    
-    Node* node1 = new Node(20);
-    Node* node2 = new Node(30);
-    Node* node3 = new Node(40);
-    Node* node4 = new Node(50);
-    Node* node5 = new Node(60);
-    Node* node6 = new Node(70);
-    Node* node7 = new Node(80);
-    Node* node8 = new Node(90);
-    Node* node9 = new Node(110);
-    Node* node10 = new Node(120);
+    // Initial Step: create queue and the push the root node into the queue
+    queue<Node *> q;
+    q.push(root);
+
+    // TRAVERSAL
+    while (!q.empty())
+    {
+        // Step 1: fetch the front element
+        Node *currNode = q.front();
+
+        //print the current Node ka data
+        cout << currNode->data << endl;
+
+        // step 2: pop the front element from the queue
+        q.pop();
+
+        /** ALWAYS CHECK  WHETHER THE LEFT / RIGHT CHILD OF CURRENT NODE IS NULL OR NOT **/
+
+        // step 3: push left child of the currNode into Queue
+        if (currNode->left != NULL)
+        {
+            q.push(currNode->left);
+        }
+
+        // step 4: push right child of the currNode into Queue
+        if (currNode->right != NULL)
+        {
+            q.push(currNode->right);
+        }
+    }
+}
+
+int main()
+{
+    Node *root = new Node(10);
+
+    Node *node1 = new Node(20);
+    Node *node2 = new Node(30);
+    Node *node3 = new Node(40);
+    Node *node4 = new Node(50);
+    Node *node5 = new Node(60);
+    Node *node6 = new Node(70);
+    Node *node7 = new Node(80);
+    Node *node8 = new Node(90);
+    Node *node9 = new Node(110);
+    Node *node10 = new Node(120);
 
     root->left = node1;
     root->right = node2;
@@ -118,7 +162,9 @@ int main()
 
     // postOrderTraversal(root);
 
-    inOrderTraversal(root);
+    // inOrderTraversal(root);
+
+    levelOrderTraversal(root);
 
     // cout << root->left->data << endl;
     // cout << root->right->data << endl;
